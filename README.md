ca7471636b66277f43c581c82bd36638f676ae21
unknown <flor20_05@hotmail.com>
2020-12-18 14:06:31 -0300

    fix icono-framer motion

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+*SE PUEDE VER EN https://firegramereact.netlify.app/
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>PetGram</h1>
       <h2>Tus fotos</h2>
       <p>Bellas miradas</p>
     </div>
