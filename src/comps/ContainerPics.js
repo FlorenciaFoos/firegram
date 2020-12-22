1bb98cbc124a24f6a0c009ad880009ff5121853f
unknown <flor20_05@hotmail.com>
2020-12-22 00:59:58 -0300

    ordeno carpeta

--- src/comps/ContainerPics.js
+++ src/comps/ContainerPics.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import UploadForm from './fotos/UploadForm';
-import ImageGrid from './fotos/ImageGrid';
-import Modal from './fotos/Modal';
+import UploadForm from './galeria/UploadForm';
+import ImageGrid from './galeria/ImageGrid';
+import Modal from './galeria/Modal';
 import Nav from './Nav'
 
 const ContainerPics = () => {
