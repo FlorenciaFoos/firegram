cfb145245d10126d0b02acc24aa0ebb5a170691f
unknown <flor20_05@hotmail.com>
2021-01-13 19:00:39 -0300

    imagenes

--- src/comps/Nav.js
+++ src/comps/Nav.js
@@ -33,16 +33,16 @@ const Nav = () => {
 
 
       <nav className="flex items-center justify-beetween flex-wrap bg-gradient-to-r from-yellow-500 via-red-500 to-pink-800 p-6">
-        <div className="flex items-center flex-shrink-0 v mr-6">
+        <div className="flex items-center flex-shrink-0 v ml-16">
           <h1 className="font-semibold text-xl tracking-tight">PetGram</h1>
         </div>
-        <div className="block md:hidden">
+        <div className="block lg:hidden">
           <button onClick={handleMenuResponsive} id='boton' className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
           </button>
         </div>
-        <div id='menu' className={`uppercase w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "hidden" : ""}`}>
-          <div className="text-sm lg:flex-grow">
+        <div id='menu' className={`text-white uppercase w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "hidden" : ""}`}>
+          <div className="flex justify-end text-sm lg:flex-grow">
             <Link to="/update-profile" className="block mt-4 lg:inline-block lg:mt-0   hover:text-white mr-4">
               Editar Perfil
           </Link>
--- src/comps/autenticacion/Login.js
+++ src/comps/autenticacion/Login.js
@@ -2,6 +2,8 @@ import React, { useRef, useState } from "react"
 
 import { useAuth } from "../../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
+import image from "../../assets/img/perro_enchufe.png"
+
 
 export default function Login() {
     const emailRef = useRef()
@@ -27,29 +29,32 @@ export default function Login() {
     }
 
     return (
-        <div className='grid h-4/5 place-items-center'>
-
-            <h2 className="text-center mb-4 text-lg	 font-semibold uppercase">Ingresar</h2>
-            {error && <div className='text-red-600 font-bold'>{error}</div>}
-            <form onSubmit={handleSubmit}>
-                <div className='w-full' id="email">
-                    <label className='uppercase tracking-wide text-black text-xs font-bold mb-2'>Email</label>
-                    <input className=' w-full bg-gray-200 focus:bg-white text-black border border-gray-200 rounded py-3 px-4 mb-3' type="email" ref={emailRef} required />
-                </div>
-                <div className=' w-full ' id="password">
-                    <label className='uppercase tracking-wide text-black text-xs font-bold mb-2'>Contraseña</label>
-                    <input className=' w-full bg-gray-200 focus:bg-white text-black border border-gray-200 rounded py-3 px-4 mb-3' type="password" ref={passwordRef} required />
-                </div>
-                <button disabled={loading} className="transform w-full bg-yellow-500 text-black font-bold py-2 px-4  hover:text-white hover:bg-pink-800 rounded-full" type="submit">
-                    Ingresar
+        <div className='grid h-screen grid-cols-2 '>
+            <div className='flex flex-col  items-center'><img className='object-center' src={image} alt="perro con enchufe" /></div>
+            <div className='flex flex-col justify-evenly items-center'>
+                <h2 className="  mb-4 text-lg	 font-semibold uppercase">Ingresar</h2>
+                {error && <div className='text-red-600 font-bold'>{error}</div>}
+                <form onSubmit={handleSubmit}>
+                    <div className='w-full' id="email">
+                        <label className='uppercase tracking-wide text-black text-xs font-bold mb-2'>Email</label>
+                        <input className=' w-full bg-gray-200 focus:bg-white text-black border border-gray-200 rounded py-3 px-4 mb-3' type="email" ref={emailRef} required />
+                    </div>
+                    <div className=' w-full ' id="password">
+                        <label className='uppercase tracking-wide text-black text-xs font-bold mb-2'>Contraseña</label>
+                        <input className=' w-full bg-gray-200 focus:bg-white text-black border border-gray-200 rounded py-3 px-4 mb-3' type="password" ref={passwordRef} required />
+                    </div>
+                    <button disabled={loading} className="transform w-full bg-blue-500 text-white font-bold py-2 px-4   hover:bg-red-500 rounded-full" type="submit">
+                        Ingresar
             </button>
-            </form>
+                </form>
 
-            <Link to="/forgot-password" className=' transform  text-center underline text-md text-grey-dark hover:scale-125'>Olvidaste la contraseña?</Link>
+                <Link to="/forgot-password" className=' transform    underline text-md text-grey-dark hover:scale-125'>Olvidaste la contraseña?</Link>
 
 
 
-            <Link to="/signup" className=' transform  text-center underline text-md text-grey-dark hover:scale-125'>Crear Cuenta</Link>
+                <Link to="/signup" className=' transform   underline text-md text-grey-dark hover:scale-125'>Crear Cuenta</Link>
+
+            </div>
 
         </div>
     )
